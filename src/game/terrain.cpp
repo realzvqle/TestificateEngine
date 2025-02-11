ae9c53ed70e6d0ce5fd2388e709198d9b4007873
realzvqle <realzvqle@gmail.com>
2025-02-10 19:02:05 -0600

    fix

--- src/game/terrain.cpp
+++ src/game/terrain.cpp
@@ -10,8 +10,8 @@ void DrawTerrain(){
     static float i = startX, zz = startZ;
     static bool init = false;
     if(init == false){
-        for (float x = startX; x < 7.0f; x += 0.2f) {
-            for (float z = startZ; z < 7.0f; z += 0.2f) {
+        for (float x = startX; x < 2.0f; x += 0.2f) {
+            for (float z = startZ; z < 2.0f; z += 0.2f) {
                 cubes.emplace_back(); 
             }
         }
@@ -21,7 +21,7 @@ void DrawTerrain(){
     for (auto& cube : cubes) {
         cube.DrawCube(Engine::RGB(0.5f, 0.5f, 0.5f, 1.0f), glm::vec3(i * 1.5f, -0.5f, zz * 1.5f), 0);
         zz += 0.2f;
-        if (zz > 7.0f) { zz = startZ; i += 0.2f; }
+        if (zz > 2.0f) { zz = startZ; i += 0.2f; }
     }
 
     i = 0.0f;
