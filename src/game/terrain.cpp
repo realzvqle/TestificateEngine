20109cf5e9ed36d9c5cc9a67036b3394bb7c06e0
realzvqle <realzvqle@gmail.com>
2025-02-10 19:43:58 -0600

    remove

--- src/game/terrain.cpp
+++ src/game/terrain.cpp
@@ -6,6 +6,7 @@
 std::vector<Cube> cubes;
 
 void DrawTerrain(){
+    std::vector<int> toRemove; 
     static float startX = -2.0f, startY = -2.0f, startZ = -2.0f; 
     static float posx = startX, posy = startY, posz = startZ;
     static bool init = false;
@@ -17,13 +18,19 @@ void DrawTerrain(){
         }
         init = true;
     }
-
-    for (auto& cube : cubes) {
-        cube.DrawCube(Engine::RGB(0.5f, 0.5f, 0.5f, 1.0f), glm::vec3(posx * 1.5f, 0.5, posz * 1.5f), 0);
+    static float time = glfwGetTime();
+    for (size_t i = 0; i < cubes.size(); ++i) {
+        if(glfwGetTime() - time >= 0.1){
+            toRemove.push_back(i);
+            time = glfwGetTime();
+        }
+        cubes[i].DrawCube(Engine::RGB(0.5f, 0.5f, 0.5f, 1.0f), glm::vec3(posx * 1.5f, 0.5, posz * 1.5f), 0);
         posz += 0.2f;
         if (posz > 2.0f) { posz = startZ; posx += 0.2f; }
     }
-
+    for (int i : toRemove) {
+        cubes.erase(cubes.begin() + i);
+    }
     posx = 0.0f;
     posz = 0.0f;
 }
